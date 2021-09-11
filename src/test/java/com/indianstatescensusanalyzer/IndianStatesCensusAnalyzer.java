862205d5de3967e2fa233b8ab828cef72b3d1f96
SRC0747 <RjSrc@0747>
2021-09-11 12:53:01 +0530

    Welcome to Indian States Census Analyzer Problem.

---
+++ src/test/java/com/indianstatescensusanalyzer/IndianStatesCensusAnalyzer.java
@@ -0,0 +1,7 @@
+package com.indianstatescensusanalyzer;
+
+public class IndianStatesCensusAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyzer");
+    }
+}

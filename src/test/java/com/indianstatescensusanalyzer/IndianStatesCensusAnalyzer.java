60c642585be4669d8e17417f9f3e695ddcbc9f7a
SRC0747 <RjSrc@0747>
2021-09-11 19:09:17 +0530

    added: Creating CSV File of Indian States Census Analyser.

--- src/test/java/com/indianstatescensusanalyzer/IndianStatesCensusAnalyzer.java
+++ src/test/java/com/indianstatescensusanalyzer/IndianStatesCensusAnalyzer.java
@@ -1,7 +1,22 @@
 package com.indianstatescensusanalyzer;
 
+import com.opencsv.*;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class IndianStatesCensusAnalyzer {
     public static void main(String[] args) {
         System.out.println("Welcome to Indian States Census Analyzer");
+        String csvFile = "CensusAnalyser.csv";
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReader(new FileReader(csvFile));
+            String[] value;
+            while ((value = csvReader.readNext()) != null) {
+                System.out.println(value[0] + " " + value[1] + " " + value[2] + " " + value[3] + " " + value[4] + " " + value[5] + " " + value[6] + " " + value[7]);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
